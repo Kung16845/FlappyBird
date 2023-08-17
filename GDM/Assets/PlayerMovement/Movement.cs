a60703a082b772cc18ddeaf340365a0141d861d7
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-08-17 21:07:12 +0700

    MovementAndAnimation

--- GDM/Assets/PlayerMovement/Movement.cs
+++ GDM/Assets/PlayerMovement/Movement.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Movement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+{   
+    public Rigidbody2D myRigidbody2D;
+    public Animator animator;
+    public float jumpForce;
+    public bool isDead = false;
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("idle", true);
+        if (!isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("jump");
+                myRigidbody2D.velocity = new Vector2(0, 0);
+                myRigidbody2D.AddForce(jumpForce * Vector2.up);
+                animator.SetTrigger("Jump");
+            }            
+        }
+        else
+        {
+            animator.SetBool("die", true);
+            animator.SetBool("idle", false);
+        }
     }
 }
